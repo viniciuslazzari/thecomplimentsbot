a6bd60479500be7652f9613606da542f4cd27b82
Vinicius Lazzari <vinilazzari028@gmail.com>
2019-12-17 15:26:18 -0300

    Version 1.0

---
+++ database/database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE compliments(
+    id              INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    compliment      VARCHAR(80) NOT NULL
+);
+
+INSERT INTO compliments(compliment) VALUES ('Você é linda(o)');
+INSERT INTO compliments(compliment) VALUES ('Seus olhos são maravilhosos');
+INSERT INTO compliments(compliment) VALUES ('Você tem o melhor sorriso');
+INSERT INTO compliments(compliment) VALUES ('Você esta tão linda(o) nessa foto de perfil');
+INSERT INTO compliments(compliment) VALUES ('Você é maravilhosa(o)');
+INSERT INTO compliments(compliment) VALUES ('Você é um presente a todos aqueles ao seu redor');
+INSERT INTO compliments(compliment) VALUES ('Eu gosto do seu estilo');
+INSERT INTO compliments(compliment) VALUES ('Você tem que estar orgulhosa de si mesma(o)');
+INSERT INTO compliments(compliment) VALUES ('Em uma escala de 1 a 10, você é 11');
+INSERT INTO compliments(compliment) VALUES ('Você atrai o que há de melhor nas pessoas');
+INSERT INTO compliments(compliment) VALUES ('Você é a razão do sorriso de alguém');
+INSERT INTO compliments(compliment) VALUES ('Obrigado por ser você mesma(o)');
+INSERT INTO compliments(compliment) VALUES ('Você é até melhor que um unicórnio, porque você é real');
+INSERT INTO compliments(compliment) VALUES ('Você é um exemplo para outras pessoas');
+INSERT INTO compliments(compliment) VALUES ('Você é tão gentil');
+INSERT INTO compliments(compliment) VALUES ('Tudo seria melhor se mais pessoas fossem como você');
+INSERT INTO compliments(compliment) VALUES ('Sua energia é tão boa e contagiante');
+INSERT INTO compliments(compliment) VALUES ('Sua risada é contagiante');
+INSERT INTO compliments(compliment) VALUES ('Seu eu interior é ainda mais bonito que o exterior');
+INSERT INTO compliments(compliment) VALUES ('Estar perto de você torna tudo melhor');
---
+++ main.py
@@ -0,0 +1,73 @@
+import tweepy
+import time
+import MySQLdb
+from keys import db
+from keys import api
+from random import randint
+
+print('Bot is online...')
+
+db = MySQLdb.connect(
+    host = db.host, 
+    user = db.user, 
+    passwd = db.passwd, 
+    db = db.db
+)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM compliments")
+
+compliments = cursor.fetchall()
+
+db.close()
+
+FILE_NAME = "last_seen_id.txt"
+
+auth = tweepy.OAuthHandler(api.CONSUMER_KEY, api.CONSUMER_SECRET)
+auth.set_access_token(api.ACCESS_KEY, api.ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def retrive_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def like_tweet(id):
+    api.create_favorite(id)
+
+def pick_random_compliment():
+    number = randint(0, len(compliments) - 1)
+    return compliments[number][1]
+
+def reply_to_tweets():
+    last_seen_id = retrive_last_seen_id(FILE_NAME)
+
+    mentions = api.mentions_timeline(
+        last_seen_id,
+        tweet_mode='extended'
+    )
+
+    for mention in reversed(mentions):
+        print("Menção feita pelo usuário @" + mention.user.screen_name)
+        print("Respondendo...")
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+
+        like_tweet(mention.id)
+        api.update_status(
+            status = pick_random_compliment(), 
+            in_reply_to_status_id = str(mention.id), 
+            auto_populate_reply_metadata = True
+        )
+
+while True:
+    reply_to_tweets()
+    time.sleep(10)
